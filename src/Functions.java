eee39c8f175983445e1f84fba246ff500347e560
VioletViola <vivian.v18723@gmail.com>
2021-08-16 21:48:24 +0100

    Practice creating functions using java.

--- src/Functions.java
+++ src/Functions.java
@@ -1,5 +1,27 @@
 public class Functions {
-    public static void main(String[] args) {
+    public static void printOut() {
+        System.out.println("printOut() function has been invoked!");
+    }
 
+    public static void printName(String fName, String lName) {
+        System.out.println("My name is " + fName + " " + lName + ".");
+    }
+
+    public static void printAge(int age) {
+        System.out.println("I'm " + age + " years old.");
+    }
+
+    public static void printAddress(String address) {
+        System.out.println("My address is " + address);
+    }
+    public static String combineAddress(String line1, String line2, String line3) {
+        return line1 + " " + line2 + " " + line3;
+    }
+
+    public static void main(String[] args) {
+        printOut();
+        printName("Nene", "Leakes");
+        printAge(57);
+        printAddress(combineAddress("444 Hotting Lane", "Yotsville", "6TY WQ3"));
     }
 }
